fd0c603fbe19c562332965a11924b87538229de8
Direta Ramandha Pratama <diretaramandhapratama@gmail.com>
2024-09-20 09:58:08 +0700

    first upgrade

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('auth.login');
 });
 
-Route::get('/login',[Controller::class,'viewlogin']);
+Route::get('/login',[AuthController::class,'ViewLogin']);
