de9c20eed409fc27b7b6acb8912a66467897bc7d
nasrielsidiq <mmuftinasrilsidiqmartono@gmail.com>
2024-10-04 08:24:29 +0700

    Add delete route

--- routes/web.php
+++ routes/web.php
@@ -21,46 +21,56 @@ Route::get('/admin/add/user/industry',[AdminController::class, 'viewAddIndustry'
 Route::post('/admin/add/user/industry',[AdminController::class, 'addIndustry']);
 Route::get('/admin/update/user/industry/{id}',[AdminController::class, 'viewUpdateIndustry']);
 Route::post('/admin/update/user/industry/{id}',[AdminController::class, 'updateIndustry']);
+Route::get('/admin/delete/user/industry/{id}',[AdminController::class, 'deleteIndustry']);
 Route::get('/admin/add/profile/industry',[AdminController::class, 'viewProfileIndustry']);
 Route::post('/admin/add/profile/industry',[AdminController::class, 'addProfileIndustry']);
 Route::get('/admin/update/profile/industry/{id}',[AdminController::class, 'viewUpdateProfileIndustry']);
 Route::post('/admin/update/profile/industry/{id}',[AdminController::class, 'updateProfileIndustry']);
+Route::get('/admin/update/profile/industry/{id}',[AdminController::class, 'deleteProfileIndustry']);
 
 Route::get('/admin/add/user/school',[AdminController::class, 'viewAddSchool']);
 Route::post('/admin/add/user/school',[AdminController::class, 'addSchool']);
 Route::get('/admin/update/user/school/{id}',[AdminController::class, 'viewUpdateSchool']);
 Route::post('/admin/update/user/school/{id}',[AdminController::class, 'updateSchool']);
+Route::get('/admin/delete/user/school/{id}',[AdminController::class, 'deleteSchool']);
 Route::get('/admin/add/profile/school',[AdminController::class, 'viewAddProfileSchool']);
 Route::post('/admin/add/profile/school',[AdminController::class, 'addProfileSchool']);
 Route::get('/admin/update/profile/school/{id}',[AdminController::class, 'viewUpdateProfileSchool']);
 Route::post('/admin/update/profile/school/{id}',[AdminController::class, 'updateSchool']);
+Route::get('/admin/delete/profile/school/{id}',[AdminController::class, 'deleteProfileSchool']);
 
 Route::get('/admin/add/user/teacher',[AdminController::class, 'viewAddTeacher']);
 Route::post('/admin/add/user/teacher',[AdminController::class, 'addTeacher']);
 Route::get('/admin/update/user/teacher/{id}',[AdminController::class, 'viewUpdateTeacher']);
 Route::post('/admin/update/user/teacher/{id}',[AdminController::class, 'updateTeacher']);
+Route::get('/admin/delete/user/teacher/{id}',[AdminController::class, 'deleteTeacher']);
 Route::get('/admin/add/profile/teacher',[AdminController::class, 'viewAddProfileTeacher']);
 Route::post('/admin/add/profile/teacher',[AdminController::class, 'addProfileTeacher']);
 Route::get('/admin/update/profile/teacher/{id}',[AdminController::class, 'viewUpdateProfileTeacher']);
 Route::post('/admin/update/profile/teacher/{id}',[AdminController::class, 'updateProfileTeacher']);
+Route::get('/admin/delete/profile/teacher/{id}',[AdminController::class, 'deleteProfileTeacher']);
 
 Route::get('/admin/add/user/advisor',[AdminController::class, 'viewAddAdvisor']);
 Route::post('/admin/add/user/advisor',[AdminController::class, 'AddAdvisor']);
 Route::get('/admin/update/user/advisor/{id}',[AdminController::class, 'viewUpdateAdvisor']);
 Route::post('/admin/update/user/advisor/{id}',[AdminController::class, 'updateAdvisor']);
+Route::get('/admin/delete/user/advisor/{id}',[AdminController::class, 'deleteAdvisor']);
 Route::get('/admin/add/profile/advisor',[AdminController::class, 'viewAddProfileAdvisor']);
 Route::post('/admin/add/profile/advisor',[AdminController::class, 'AddProfileAdvisor']);
 Route::get('/admin/update/profile/advisor/{id}',[AdminController::class, 'viewUpdateProfileAdvisor']);
 Route::post('/admin/update/profile/advisor/{id}',[AdminController::class, 'updateProfileAdvisor']);
+Route::get('/admin/delete/profile/advisor/{id}',[AdminController::class, 'deleteProfileAdvisor']);
 
 Route::get('/admin/add/user/student',[AdminController::class, 'viewAddStudent']);
 Route::post('/admin/add/user/student',[AdminController::class, 'addStudent']);
 Route::get('/admin/update/user/student/{id}',[AdminController::class, 'viewUpdateStudent']);
 Route::post('/admin/update/user/student/{id}',[AdminController::class, 'updateStudent']);
+Route::get('/admin/delete/user/student/{id}',[AdminController::class, 'deleteStudent']);
 Route::get('/admin/add/profile/student',[AdminController::class, 'viewAddProfileStudent']);
 Route::post('/admin/add/profile/student',[AdminController::class, 'addProfileStudent']);
 Route::get('/admin/update/profile/student/{id}',[AdminController::class, 'viewUpdateProfileStudent']);
 Route::post('/admin/update/profile/student/{id}',[AdminController::class, 'updateProfileStudent']);
+Route::get('/admin/delete/profile/student/{id}',[AdminController::class, 'deleteProfileStudent']);
 
 //teacher-page
 Route::get('/dashboard-page',[Teacher::class, 'teacher']);
